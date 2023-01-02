249380e543c3143bc8db57fd947b917f795a247b
Zeeshan-2k1 <ashraf2k1zeeshan@gmail.com>
2023-01-02 12:36:40 +0530

    fix: fixed dummy integrations

--- integrations/DummyIntegration.bundle.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";DummyIntegrations={name:"Dummy Integrations",description:"This is a dummy integration description for testing",category:["testing"],config:["account_id"],event:t=>{console.log("DummyIntegration Event Callback: ",t)}}})();
--- src/DummyIntegration/index.js
+++ src/DummyIntegration/index.js
@@ -1,9 +1,11 @@
 import DummyIntegrationEvent from './EventHandler';
 
-export default DummyIntegrations = {
+const DummyIntegrations = {
   name: 'Dummy Integrations',
   description: 'This is a dummy integration description for testing',
   category: ['testing'],
   config: ['account_id'],
   event: DummyIntegrationEvent,
 };
+
+export default DummyIntegrations;
